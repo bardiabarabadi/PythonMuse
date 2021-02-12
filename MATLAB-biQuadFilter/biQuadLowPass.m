367309a2a2b9818cecde06ff2d5c4949681b5966
bardiabarabadi <R@mz3GitHub>
2021-02-12 15:59:20 -0800

    Added matlab functions from Krigolson's repository (https://github.com/krigolson)

---
+++ MATLAB-biQuadFilter/biQuadLowPass.m
@@ -0,0 +1,23 @@
+function filterCoefficients = biQuadLowPass(frequency,sampleRate,bandWidth)
+
+    % create the low pass coefficients for a biquad filter
+
+    omega = 2 * pi * frequency / sampleRate;
+    sn = sin(omega);
+    cs = cos(omega);
+    alpha = sn * sinh(log(2) / 2 * bandWidth * omega / sn);
+
+    b0 = (1 - cs) / 2;
+    b1 = 1 - cs;
+    b2 = (1 - cs) / 2;
+    a0 = 1 + alpha;
+    a1 = -2 * cs;
+    a2 = 1 - alpha;
+
+    filterCoefficients(1) = b0/a0;
+    filterCoefficients(2) = b1/a0;
+    filterCoefficients(3) = b2/a0;
+    filterCoefficients(4) = a1/a0;
+    filterCoefficients(5) = a2/a0;
+    
+end
